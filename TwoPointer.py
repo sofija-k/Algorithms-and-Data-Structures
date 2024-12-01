7a168d0ae3e0e490d9c7f1fc8c307ace392c2873
Sofija <sofija@Sofijas-MacBook-Pro.local>
2024-12-01 21:27:23 +0100

    Implemented Solution to Three Sum Problem

--- TwoPointer.py
+++ TwoPointer.py
@@ -20,6 +20,7 @@ def isPalindrome(self, s: str) -> bool:
             r -= 1
         return True
 
+
 '''
 Two Integer Sum II - Input Array is Sorted
 link: https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
@@ -36,4 +37,31 @@ def twoSum(self, numbers: List[int], target: int) -> List[int]:
                 l += 1
             else:
                 r -= 1 
-        
+                
+'''
+Three Sum
+link: https://leetcode.com/problems/3sum/description/
+'''
+def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        for i, val in enumerate(nums):
+            if i > 0 and val == nums[i-1]:
+                continue
+            
+            l, r = i + 1, len(nums)-1
+
+            while l < r:
+                if nums[l] + nums[r] + val < 0:
+                    l += 1
+                elif nums[l] + nums[r] + val > 0:
+                    r -= 1
+                else:
+                    res.append([val, nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while nums[l] == nums[l-1] and l<r:
+                        l += 1
+        return res
+                        
