d4e16b161776e42d2a822cd221ba0d4ef5720cde
Sofija <sofija@Sofijas-MacBook-Pro.local>
2024-12-08 20:03:52 +0100

    ImplementedLongest Substring without Duplicates utilizing Sliding Window Algo Approach

--- sliding_window.py
+++ sliding_window.py
@@ -1,3 +1,4 @@
+from typing import List
 '''
 Best Time to Buy and Sell Stock
 link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
@@ -14,3 +15,22 @@ def maxProfit(self, prices: List[int]) -> int:
         r += 1 
     
     return max_profit
+
+'''
+Longest Substring Without Repeating Characters
+link: https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+'''
+def lengthOfLongestSubstring(self, s: str) -> int:
+    l = 0
+    substring = {}
+    length = 0
+    
+    for r in range(len(s)):
+        if s[r] in substring:
+            l = max(l, substring[s[r]] + 1)
+            
+        substring[s[r]] = r
+        length = max(length, r-l+1)
+        print(substring, length)
+
+    return length
